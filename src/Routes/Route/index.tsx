2ecfa42a68a4bd3f093e1ea531a3f047dc5523c7
puddingh00 <154326309+puddingh00@users.noreply.github.com>
2025-03-06 16:04:17 +0000

    Change Title

--- src/Routes/Route/index.tsx
+++ src/Routes/Route/index.tsx
@@ -1,8 +1,51 @@
 export const RootRoute = () => {
     return <div>{data.map((item) => {
-        return <div>{item}</div>
+        return <div className="">{item.name}
+        <img src={item.image}/>
+        </div>
     })}</div>
     }
-    const data = ["Charmender 00", "Lamin01", "Cossu_Elbeff02", "Bobattolo03"];
-
-
+    const data = [
+        {
+          id: 1,
+          image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+          name: "Bulbasaur",
+          types: ["grass", "poison"],
+        },
+        {
+          id: 4,
+          image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+          name: "Charmander",
+          types: ["fire"],
+        },
+        {
+          id: 7,
+          image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+          name: "Squirtle",
+          types: ["water"],
+        },
+        {
+          id: 10,
+          image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10.png",
+          name: "Caterpie",
+          types: ["bug"],
+        },
+        {
+          id: 25,
+          image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+          name: "Pikachu",
+          types: ["electric"],
+        },
+        {
+          id: 74,
+          image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/74.png",
+          name: "Geodude",
+          types: ["rock", "ground"],
+        },
+      ];
