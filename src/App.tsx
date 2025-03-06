3836a3f94dafb74ae88b7c9e6ba5e1a32061f7fe
puddingh00 <154326309+puddingh00@users.noreply.github.com>
2025-03-06 11:41:47 +0000

    Change Title

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import viteLogo from "/vite.svg";
 
 export const App = () => {
   const [count, setCount] = useState(0);
-  const [title, setTitle ] = useState("sciaog");
-  return (
+  const [title, setTitle ] = useState("sciaogo");
+  return (  
     <div className="h-dvh flex flex-col items-center justify-center">
       <div className="bg-white p-8 rounded-md shadow-lg">
         <h1 className="text-center font-bold text-3xl text-blue-400 mb-4">
